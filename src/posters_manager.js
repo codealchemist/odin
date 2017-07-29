7f465cf660aff76782d754bd58c35725b19a29e6
Santiago Carullo <scarullo@elementum.com>
2017-07-29 20:17:25 -0300

    Fix posters manager

--- src/posters_manager.js
+++ src/posters_manager.js
@@ -4,7 +4,7 @@ const GoogleImages = require('google-images');
 const client = new GoogleImages(config.google_cse.cse_id, config.google_cse.api_key);
 
 const searchPoster = (movieName) => {
-  client.search(`${movieName} movie poster`)
+  return client.search(`${movieName} movie poster`)
     .then((images) => images.find(image => image.type == "image/jpeg").url)
 }
 
